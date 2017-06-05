6a53d3b07f1bc94b089e311968e2f5209b6cc630
Alex Carney <alcarneyorg@gmail.com>
2017-06-05 19:13:04 +0100

    Work on Layered and regular Images
    - Added Layered Image class
    - Implemented __getitem__ to return Image class when appropriate
    - Implemented __setitem__ and added a simple __repr__
    
    - Implemented __call__ for the Image class to perform the work
      of the old imgmap function

--- mage/img.py
+++ mage/img.py
@@ -3,6 +3,89 @@ import matplotlib.pyplot as plt
 import PIL as P
 
 
+class LayeredImage:
+    """
+    A representation of image data, making use of layers
+    """
+
+    def __init__(self, width=None, height=None, layers=None,
+                 background=(0, 0, 0, 0), pixels=None):
+        """
+        Layered Image Constructor
+
+        There are two ways to construct a layered image, the first is to
+        specify the width, height and number of layers you wish to have
+        along with a fill colour.
+
+        Alternatively you can provide a numpy array with the shape
+        (layers, height, width, 4) that can be used as the image.
+
+        It's important to note that if you provide the pixels argument
+        it will override all the others.
+
+        Parameters
+        ----------
+        width : int, optional
+            The width of the image in pixels. Default None
+        height : int, optional. Default None
+            The height of the image in pixels
+        layers : int, optional
+            The number of layers to include in the image.
+            Default None.
+        background : 4-tuple, optional
+            The fill RGBA colour of the image, each value can be in the range
+            0-255 (default is (0, 0, 0, 0) - fully transparent black)
+        pixels : numpy array, optional
+            If you already have a numpy array you want to use as an image
+            simply pass it in here. **Note:** The array must have the shape
+            (height, width, 4). Default value None.
+        """
+
+        if pixels is not None:
+            shape = pixels.shape
+
+            if len(shape) != 4 or shape[-1] != 4:
+                raise ValueError("Pixels array must have shape: "
+                                 "(layers, height, width, 4)")
+
+            self.pixels = pixels
+        else:
+            if width is None or height is None or layers is None:
+                raise ValueError("If no pixels array is given you must "
+                                 "specify number of layers and the widht and "
+                                 "height of the image.")
+
+            self.pixels = np.full((layers, height, width, 4), background,
+                                  dtype=np.uint8)
+
+    @classmethod
+    def fromarray(cls, px):
+        return cls(pixels=px)
+
+    def __getitem__(self, index):
+        """
+        Allows for the use of img[] syntax.
+
+        We let numpy handle most of the indexing, but sometimes we could
+        be left with a single layer image in which case we wrap it in an image
+        class.
+        """
+        px = self.pixels[index]
+        shape = px.shape
+
+        if len(shape) == 3:
+            return Image.fromarray(px)
+        else:
+            return LayeredImage.fromarray(px)
+
+    def __setitem__(self, index, value):
+        self.pixels[index] = value
+
+    def __repr__(self):
+        shape = self.pixels.shape
+        return "%ix%i Layered Image with %i layers" % (shape[1], shape[2], shape[0])
+
+
 class Image:
     """
     A basic representation of image data
@@ -25,8 +108,8 @@ class Image:
         Or you can provide a numpy array with the shape (height, width, 4)
         which you can use as an image.
 
-        It's important to note that if you provide the numpy it will override
-        all the other arguments
+        It's important to note that if you provide the pixels argument it will
+        override all the others.
 
         Parameters
         ----------
@@ -47,7 +130,7 @@ class Image:
             shape = pixels.shape
 
             if len(shape) != 3 or shape[-1] != 4:
-                raise ValueError("Pixels array must have shape"
+                raise ValueError("Pixels array must have shape:"
                                  " (height, width, 4)")
 
             self.pixels = pixels
@@ -56,48 +139,46 @@ class Image:
                 raise ValueError("If no pixels array is given you"
                                  " must specify a width and height")
 
-            self.pixels = np.full((width, height, 4), background,
+            self.pixels = np.full((height, width, 4), background,
                                   dtype=np.uint8)
 
     @classmethod
     def fromarray(cls, px):
-        """
-        A nice way to get an image from an array of pixels
-        """
         return cls(pixels=px)
 
     def __getitem__(self, index):
-        """
-        Allows for the use of `img[]` syntax.
-
-        Since the underlying data is simply a numpy array, we pass off the
-        given index to that
-        """
         return Image.fromarray(self.pixels[index])
 
     def __setitem__(self, index, value):
-        """
-        Allows for the use of `img[] = x` syntax
-
-        Again, since our underlying data structure is a numpy array, we let
-        numpy handle this for us
-        """
         self.pixels[index] = value
 
     def __repr__(self):
-        """
-        Prints a nice string representation of the image
-        """
         return '%ix%i Image' % self.pixels.shape[0:2]
 
     def __or__(self, other):
-        """
-        "OR" Implementation
-
-        Like a union, combines the output of the two images
-        """
-
+        # TODO: Implement boolean OR for images
         return 1
+
+    def __call__(self, f):
+
+        jmax, imax, _ = self.pixels.shape
+
+        for i in range(imax):
+
+            # Calculate the percentage across the image the
+            # current pixel is
+            I = i / imax
+
+            for j in range(jmax):
+
+                # Calculate the percentage down the image the
+                # current pixel is
+                J = j / jmax
+
+                res = f(I, J)
+
+                if res is not None:
+                    self.pixels[j, i] = res
 
     def show(self):
         """
