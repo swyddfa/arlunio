79c2743f3cafbddf66f6001a48febf8d80d6d718
Alex Carney <alcarneyorg@gmail.com>
2017-06-21 19:19:55 +0100

    Added translate decorator
    - Added a new decorator which allows the translation and rotation of
      domains onto the final image
    - Removed redundant docstring

--- mage/coords.py
+++ mage/coords.py
@@ -1,38 +1,6 @@
-from math import sqrt, atan2
+import numpy as np
+from math import sqrt, atan2, sin, cos
 
-
-"""
-An object is simply the pairing of two things. A domain, and the rule.
-
-The domain is a transformation which takes two percentages i and j which
-represent the percentage 'through' an image the current pixel is and maps it
-into the mathematical domain that the rule is defined on.
-
-This serves two purposes, one the rule can be mapped over images of
-different sizes with no change, it also simplifies the process of applying
-transformations and conversions.
-
-We can split the domain up further, since the object will always eventually
-be mapped onto a rectangular domain - the image, we can consider the max/min
-values of x and y as their own thing.
-
-Then we have the transformation, the function which takes the percentages
-i and j and converts them to the input variables x1, x2 which we define the
-rule for.
-
-Therefore when defining the decorators they must produce an object with
-the following form:
-
-{
-  'bounds': [(minX, maxX), (minY, maxY)]
-  'transformation': <lambda: i, j> -> x1, x2
-  'rule': <lambda x1, x2> -> RGB/RGBA
-}
-
-Then if all decorators assume this form, then we *should*, be able to
-compose them nicely.
-
-"""
 
 def cartesian(X=[-1, 1], Y=[-1, 1]):
     """
@@ -74,7 +42,6 @@ def extend_periodically(X=[-1, 1], Y=[-1, 1]):
     and extends it to an arbitrary domain by looping it back on itself
     """
 
-
     xmin, xmax = X
     xlength = xmax - xmin
     ymin, ymax = Y
@@ -98,6 +65,29 @@ def extend_periodically(X=[-1, 1], Y=[-1, 1]):
             return f(x, y)
 
         return extended
+
+    return wrapper
+
+
+def translate(X=[0, 0], r=0):
+    """
+    A function decorator which allows for easy translation of a
+    domain onto the final image.
+    """
+    def wrapper(f):
+
+        # We need to shift values in the opposite direction
+        # to achieve the intiuitive behavior
+        M = np.array([[cos(-r), -sin(-r), -X[0]],
+                      [sin(-r),  cos(-r), -X[1]],
+                      [0, 0, 1]])
+
+        def transformed(x, y):
+
+            (x, y, _) = np.dot(M, np.array([x, y, 1]))
+            return f(x, y)
+
+        return transformed
 
     return wrapper
 
