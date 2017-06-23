38ea001d20c878385134e1f01b5c29db17791370
Alex Carney <alcarneyorg@gmail.com>
2017-06-23 16:47:11 +0100

    Addded the new stuff to __init__.py

--- mage/__init__.py
+++ mage/__init__.py
@@ -2,4 +2,5 @@ from .coords import cartesian, polar, extend_periodically, translate
 from .color import hex_to_rgb
 from .image import Image
 from .motion import Projectile
+from .prims import circle, ellipse, rectangle, square, thicken
 from .time import animate
