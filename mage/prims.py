7152600f25dab9fe7bc589672dbe80f322955f31
Alex Carney <alcarneyorg@gmail.com>
2017-06-23 16:23:40 +0100

    More primitives
    - I finally have it set in my mind how this can work, all
      primitive/shape functions should be of the form (x, y) -> bool
      so you can easily combine them with and , or, not etc.
    - Converted `ellipse` and `circle` primitives to match this
    - Added new rectangle and square primitives

--- mage/prims.py
+++ mage/prims.py
@@ -1,12 +1,12 @@
 
-def ellipse(x0, y0, a, b):
+def ellipse(x0, y0, a, b, r, pt=0.2, fill=False):
     """
     An ellipse can be defined in the following manner:
 
         1/a(x - x0)^2 + 1/b(y - y0)^2 = r^2
 
     This function returns a function in (x, y) and will
-    evaluate the LHS of the above equation
+    return true if the given point is in the ellipse
 
     Arguments:
     ----------
@@ -21,23 +21,52 @@ def ellipse(x0, y0, a, b):
     b : float
         This controls how elongated the ellipse is in the y
         direction
+    r : float
+        This represents the radius of the ellipse
+    pt : float, optional
+        Represents the thickness of the lines of the ellipse.
+        Default: 0.2
+    fill : bool, optional
+        Fill the ellipse rather than outline it
+        Default: False
+        **Note:** If fill is true, this function will ignore the value
+        of pt
     """
 
-    def mk_ellipse(x, y):
+    lhs = lambda x, y: 1/a*(x - x0)**2 + 1/b*(y - y0)**2
 
-        return 1/a*(x - x0)**2 + 1/b*(y - y0)**2
+    if fill:
 
-    return mk_ellipse
+        def ell(x, y):
+
+            if lhs(x, y) <= r**2:
+                return True
+
+            return False
+
+        return ell
+
+    else:
+
+        def ipse(x, y):
+
+            val = lhs(x, y)
+
+            if val <= (r + pt)**2 and val >= (r - pt)**2:
+                return True
+
+            return False
+        return ipse
 
 
-def circle(x0, y0):
+def circle(x0, y0, r, *args, **kwargs):
     """
     A circle can be defined in the following manner:
 
         (x - x0)^2 + (y - y0)^2 = r^2
 
     This function returns a function in (x, y) and
-    will evaluate the LHS of the above equation
+    will return true if the point is in the circle
 
     Arugments:
     ----------
@@ -46,9 +75,19 @@ def circle(x0, y0):
         This is the x coordinate of the circle's center
     y0 : float
         This is the y coordinate of the circle's center
+    r : float
+        This represents the radius of the ellipse
+    pt : float, optional
+        Represents the thickness of the lines of the circle.
+        Default: 0.2
+    fill : bool, optional
+        Fill the circle rather than outline it
+        Default: False
+        **Note:** If fill is true, this function will ignore the value
+        of pt
     """
 
-    return ellipse(x0, y0, 1, 1)
+    return ellipse(x0, y0, 1, 1, r, *args, **kwargs)
 
 
 def thicken(f, pt):
@@ -79,3 +118,91 @@ def thicken(f, pt):
             return False
 
     return test_func
+
+
+def rectangle(x0, y0, width, height, pt=0.2, fill=False):
+    """
+    Returns a function in (x, y) which returns true
+    when the given point in in/on the rectangle defined by the
+    arguments.
+
+    Arguments:
+    ----------
+
+    x0 : float
+        Represents the x-coordinate of the rectangle's center
+    y0 : float
+        Represents the y-coordinate of the rectangle's center
+    width : float
+        Represents the width of the rectangle
+    height : float
+        Represents the height of the rectangle
+    pt : float, optional
+        Represents the thickness of the lines of the rectangle.
+        Default: 0.2
+    fill : bool, optional
+        Fill the rectangle rather than outline it
+        Default: False
+        **Note:** If fill is true, this function will ignore the value
+        of pt
+    """
+    left = x0 - (width / 2)
+    right = x0 + (width / 2)
+    top = y0 + (height / 2)
+    bottom = y0 - (height / 2)
+
+    def rect(x, y):
+
+        if x >= left and x <= right and\
+           y >= bottom and y <= top:
+            return True
+
+        return False
+
+    if fill:
+        return rect
+
+    def small(x, y):
+
+        if x >= left + pt and x <= right - pt and\
+           y >= bottom + pt and y <= top - pt:
+            return True
+
+        return False
+
+    def test(x, y):
+
+        if rect(x, y) and not small(x, y):
+            return True
+
+        return False
+
+    return test
+
+
+def square(x0, y0, size, *args, **kwargs):
+    """
+    Returns a function in (x, y) which returns true
+    when the given point in in/on the square defined by the
+    arguments.
+
+    Arguments:
+    ----------
+
+    x0 : float
+        Represents the x-coordinate of the rectangle's center
+    y0 : float
+        Represents the y-coordinate of the rectangle's center
+    size : float
+        Represents the size of the rectangle
+    pt : float, optional
+        Represents the thickness of the lines of the rectangle.
+        Default: 0.2
+    fill : bool, optional
+        Fill the rectangle rather than outline it
+        Default: False
+        **Note:** If fill is true, this function will ignore the value
+        of pt
+
+    """
+    return rectangle(x0, y0, size, size, *args, **kwargs)
