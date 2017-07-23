a5991085cc0d5691c5ff6ff1b0c6df94c5c68c52
Alex Carney <alcarneyorg@gmail.com>
2017-07-23 20:11:42 +0100

    Updated Imports

--- mathemage/mage/__init__.py
+++ mathemage/mage/__init__.py
@@ -1,7 +1,8 @@
 from .coords import cartesian, extend_periodically, translate, reflect, polar
 from .color import hex_to_rgb
-from .image import Image
+from .image import Image, LayeredImage, TiledImage
 from .motion import Projectile, driver
+from .objects import TileSet
 from .prims import between, circle, ellipse, rectangle, square, thicken,\
                    Puppet
 from .time import animate
