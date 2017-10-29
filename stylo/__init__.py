2e96ff6c6fe07ba49031caed353485080c89ebd1
Alex Carney <alcarneyme@gmail.com>
2017-10-29 08:20:41 +0000

    Cleanup
    - Removed code that was either broken or I felt had no place being in
    the core part of stylo.
    - This hopefully also means that this will now work on py3.5

--- stylo/__init__.py
+++ stylo/__init__.py
@@ -2,10 +2,8 @@ from .coords import cartesian, extend_periodically, translate, reflect,\
                     polar, mk_domain
 from .color import hex_to_rgb
 from .image import Image, LayeredImage, TiledImage
-from .motion import Projectile
 from .objects import TileSet
-#from .prims import between, circle, ellipse, rectangle, square,\
-#                   Puppet
+from .prims import between, circle, ellipse, rectangle, square
 from .time import animate
 
 from .version import __version__
