01a9a960f7f9a31ad381e75e83586b33ff88bde8
Alex Carney <alcarneyme@gmail.com>
2017-10-30 16:28:19 +0000

    Exposing missing objects

--- stylo/__init__.py
+++ stylo/__init__.py
@@ -2,6 +2,8 @@ from .coords import cartesian, extend_periodically, translate, reflect,\
                     polar, mk_domain
 from .color import hex_to_rgb
 from .image import Image, LayeredImage, TiledImage
+from .interpolate import sampled, Sampler, Channel, Driver,\
+                        linear, quadratic_ease_in, quadratic_ease_out
 from .objects import TileSet
 from .prims import between, circle, ellipse, rectangle, square
 from .time import animate
