fe7fffc45f53d8fbfcd756ae47d0405d8653f8a9
Alex Carney <alcarneyme@gmail.com>
2017-12-27 21:02:06 +0000

    Added a Path object, which should give us reasonable speed for line art

--- stylo/prims.py
+++ stylo/prims.py
@@ -1,4 +1,5 @@
 from math import floor
+from scipy.optimize import newton
 
 
 def ellipse(x0, y0, a, b, r, pt=0.2, fill=False):
@@ -248,3 +249,65 @@ def square(x0, y0, size, *args, **kwargs):
         if the point is in the square defined by the parameters above
     """
     return rectangle(x0, y0, size, size, *args, **kwargs)
+
+
+class Path:
+    """
+    This models a parametric path in 2D space. That is a function
+    t |-> (x(t), y(t)) for all t in some interval, usually [0,1]
+
+    By giving this object the functions x(t), y(t), a thickness that we
+    will call pt and the domain in which we take values for t this object
+    provides you with an easy method of drawing plane paths!
+    """
+
+    def __init__(self, Xt, Yt, pt=0.02, domain=(0,1)):
+        self._Xt = Xt
+        self._Yt = Yt
+        self._pt = pt
+        self._domain = domain
+
+    def __call__(self, x, y):
+        """
+        Given a point (x, y) this will return true
+        if the point is on or near the curve
+        """
+
+        # One way we could do this is to simply compute the
+        # distance of the point to the curve directly - however
+        # this approach is very computationally expensive as it means
+        # sampling the curve at enough points to get a good approximation
+        # and then performing the distance calculation against the hundreds
+        # of points... for each point in the image.... Not good
+
+        # However we can use the parametric nature to our advantage. Since
+        # we have:
+        # x = x(t)
+        # y = y(t)
+        # We can use these to ask a different question
+        # |x - x(t)| < pt?
+        # |y - y(t)| < pt?
+        # Can we find a t in our domain such that the above two relations
+        # are both simulataneously true?
+
+        # To do this we will use the newton() root finding method from scipy
+        # to find a t s.t. x - x(t) is close to zero (and thus less than pt)
+        # First we need to give a guess on where we think the root will be
+        # the midpoint is as good as any I suppose
+        a, b = self._domain
+        guess = (a + b) / 2
+
+        # Now we will find the root
+        t = newton(lambda t: x - self._Xt(t), guess)
+
+        # We will verify it's actually in the domain
+        if not (a <= t <= b):
+            return False
+
+        # Now we will check that the same t also gets out curve to within
+        # pt of the y coordinate
+        if abs(y - self._Yt(t)) <= self._pt:
+            return True
+
+        # I guess it didn't work then
+        return False
