bcf9c0fe9c04f1a33fe08aa43ea838f4bdff6193
Alex Carney <alcarneyme@gmail.com>
2018-07-16 21:06:50 +0100

    Initial stab at making working with time easier

--- stylo/time.py
+++ stylo/time.py
@@ -1,4 +1,7 @@
+import numpy as np
 import multiprocessing as mp
+
+from math import floor
 
 
 def animate(f, start=0, stop=10, fps=25, frames=None):
@@ -23,3 +26,41 @@ def animate(f, start=0, stop=10, fps=25, frames=None):
     pool.map(f, range(num_frames))
 
     pool.close()
+
+
+class Timeline:
+    """Easy time utilities."""
+
+    def __init__(self, end, fps=25):
+        self._end = end
+        self._fps = fps
+
+    def __contains__(self, t):
+        return t >= 0 and t <= self.end
+
+    def __len__(self):
+        return floor(self.end * self.fps)
+
+    def __iter__(self):
+        time = np.linspace(0, self._end, self._end * self._fps)
+        frames = range(len(self))
+
+        timesteps = [(f + 1, t) for f, t in zip(frames, time)]
+        return iter(timesteps)
+
+    def __call__(self, func, multi_process=False):
+        if not multi_process:
+            for f, t in self:
+                func(f, t)
+
+        else:
+            pool = mp.Pool()
+            pool.starmap(func, self)
+
+    @property
+    def end(self):
+        return self._end
+
+    @property
+    def fps(self):
+        return self._fps
