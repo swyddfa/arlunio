61729cd2da28ccd2fe84785edebab7d1687d3da6
Alex Carney <alcarneyme@gmail.com>
2018-07-29 19:34:23 +0100

    Add standard strategy definitions

--- tests/helpers/strategies.py
+++ tests/helpers/strategies.py
@@ -4,8 +4,9 @@ It defines the following strategies:
 
 - :code:`dimension`: Represents a size or dimension e.g. for numpy arrays image
   sizes etc.
+- :code:`real`: Represents a real number in the range +/-1 million
 """
-from hypothesis.strategies import integers
+from hypothesis.strategies import integers, floats
 
-
+real = floats(min_value=-1e6, max_value=1e6)
 dimension = integers(min_value=2, max_value=1024)
