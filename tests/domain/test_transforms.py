c58a5da8f30f4d4a452baa5db7aa10eec6286af0
Alex Carney <alcarneyme@gmail.com>
2018-08-12 16:32:13 +0100

    Adding tests for the Translate domain

--- tests/domain/test_transforms.py
+++ tests/domain/test_transforms.py
@@ -1,10 +1,12 @@
-from unittest import TestCase
-
 import pytest
-from tests.helpers.domain import BaseDomainTest
+import numpy.testing as npt
+from hypothesis import given
+from unittest import TestCase
 
 from stylo.domain.square import UnitSquare
 from stylo.domain.transforms import Translation
+from tests.helpers.domain import BaseDomainTest
+from tests.helpers.strategies import dimension, real
 
 
 @pytest.mark.domain
@@ -16,3 +18,39 @@ class TestSquareDomain(TestCase, BaseDomainTest):
         # We just need any instance of the translation to test the interface
         # with, we'll get to the details ourselves in the test cases here.
         self.domain = Translation(UnitSquare(), 1, 1)
+
+    @given(width=dimension, height=dimension, dx=real)
+    def test_dx_affects_x_values(self, width, height, dx):
+        """Ensure that the :code:`dx` shifts each value by the negative of the value
+        given by the user so that the translation is intuitive. Also ensure that only
+        the :code:`x` values are affected."""
+
+        domain = UnitSquare()
+        xs, ys = domain["xy"](width, height)
+
+        translated = Translation(domain, dx=dx, dy=0)
+        t_xs, t_ys = translated["xy"](width, height)
+
+        # The y values should be unaffected.
+        npt.assert_array_equal(ys, t_ys)
+
+        difference = xs - t_xs
+        npt.assert_array_equal(difference, -dx)
+
+    @given(width=dimension, height=dimension, dy=real)
+    def test_dy_affects_y_values(self, width, height, dy):
+        """Ensure that the :code:`dy` shifts each value by the negative of the value
+        given by the user so that the translation is intuitive. Also ensure that only
+        the :code:`y` values are affected."""
+
+        domain = UnitSquare()
+        xs, ys = domain["xy"](width, height)
+
+        translated = Translation(domain, dx=0, dy=dy)
+        t_xs, t_ys = translated["xy"](width, height)
+
+        # The x values should be unaffected
+        npt.assert_array_equal(xs, t_xs)
+
+        difference = ys - t_ys
+        npt.assert_array_equal(difference, -dy)
