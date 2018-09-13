b2d7d635289a7c8c6f0c091cede37e85dc9108fd
Alex Carney <alcarneyme@gmail.com>
2018-09-13 19:35:32 +0100

    Update Pipfile to include extra dev dependencies

--- scripts/devenv-setup.sh
+++ scripts/devenv-setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo -e "==> Creating virtual environment and installing dependencies\n"
 pipenv install --dev
+
+echo -e "\n\n==> Running a few tests and building the documentation"
+echo -e "    This can take a few minutes\n"
+pipenv run tox -e docs-build
