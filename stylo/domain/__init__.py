6f1b0466d2c2469fdbd401f48a7f81fb40d6c1f6
Alex Carney <alcarneyme@gmail.com>
2018-09-22 17:36:28 +0100

    Add `get_real_domain` function
    - Added a function :code:`get_real_domain` that given a width and height of an
      image plus a scale, will construct a new instance of
      :code:`RectangularDomain` with the same aspect ratio as the image, with the
      scale corresponding to the length of the interval :math:`[ymin, ymax]`.
    
    - The constructed domain has the point :math:`(0,0)` at its centre
    
    - Removed the ability to use domains in a pipeline

--- stylo/domain/__init__.py
+++ stylo/domain/__init__.py
@@ -2,5 +2,5 @@ from ._factory import define_domain
 
 RealDomain = define_domain("RealDomain", "x,y,r,t")
 
-from .rectangular import RectangularDomain  # noqa: F401
+from .rectangular import RectangularDomain, get_real_domain  # noqa: F401
 from .square import SquareDomain, UnitSquare  # noqa: F401
