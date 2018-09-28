7aaed915d6bbf0e414c585a5fd462fb2193f09ac
Vinícius Aguiar <vinoba_@hotmail.com>
2018-09-28 19:47:36 -0300

    Add Triangle class to __init__.py

--- stylo/shape/__init__.py
+++ stylo/shape/__init__.py
@@ -1,2 +1,2 @@
-from .basic import Rectangle, Square, Ellipse, Circle  # noqa: F401
+from .basic import Rectangle, Square, Ellipse, Circle, Triangle  # noqa: F401
 from .shape import Shape  # noqa: F401
