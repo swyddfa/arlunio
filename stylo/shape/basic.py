fe90a934daf474ace7301a7ca4c915685faf2ed2
Vinícius Aguiar <vinoba_@hotmail.com>
2018-09-28 22:50:00 -0300

    Make the docs a little more explicit

--- stylo/shape/basic.py
+++ stylo/shape/basic.py
@@ -103,7 +103,7 @@ class Square(Rectangle):
 
 class Triangle(Shape):
     """
-    A Triangle can be defined by picking three non-collinear points :math:`(a, b, c)`.
+    A Triangle can be defined by picking three non-collinear points :math:`(a, b, c)` in the form of a tuple each.
     """
     def __init__(self, a, b, c):
         self.a = a
