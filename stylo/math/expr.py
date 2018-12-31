85e471a0f72ab392f09c6124b262e079f6a582b9
Alex Carney <alcarneyme@gmail.com>
2018-12-31 18:07:36 +0000

    Protoyping the "new world".
    - I'm aiming to get a 1-1 correspondance between Python code that is
      currently written in shape definitions and a data representation of
      that code.
    - This lays the groundwork for an initial attempt at this, by creating a
      family of `StyExpr` (Stylo Expression) objects that behave in an
      identical manner to numbers and numpy arrays that the user is used to
      working with. (Allowing user code to be unchanged!) The user
      constructs a data structure representing the shape definition that we
      can then work with behind the scenes.
    - Currently this structure can only be `eval()`ed which is a very naive
      attempt at interpreting this structure to produce a result - allowing
      the library to continue to function as before (although probably with
      a performace hit)
    - However it will also introduce the possibility of:
      + Emitting (an optimised?) Python AST to be executed
      + Representing shapes as other formats such as JSON or a lisp-like
        syntax
      + Even the potential for other implementations - rust + web assembly
        anyone? :D

---
+++ stylo/math/expr.py
@@ -0,0 +1,83 @@
+"""This module implements the groundwork for how stylo is able
+to represent and evaluate expressions in multiple formats.
+"""
+import operator
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class StyExpr(ABC):
+    """This class is responsible for handling all details of converting an
+    expression to/from the various formats.
+
+    Implementing classes are responsible for the details.
+    """
+
+    def __add__(self, other):
+        return StyPlus(self, other)
+
+    def __radd__(self, other):
+        return StyPlus(other, self)
+
+    @abstractmethod
+    def eval(self):
+        pass
+
+
+class StyConst(StyExpr):
+    def __init__(self, const):
+        self.const = const
+
+    def __repr__(self):
+        return repr(self.const)
+
+    def eval(self):
+        return self.const
+
+
+def _define_binary_op(name, symbol, impl):
+    """This function declares a binary operation."""
+
+    class BinaryOp(StyExpr):
+        def __init__(self, a, b):
+            self.a = a
+            self.b = b
+
+        def __repr__(self):
+            a = repr(self.a)
+            b = repr(self.b)
+
+            return "({} {} {})".format(symbol, a, b)
+
+        def eval(self):
+            a = self.a
+            b = self.b
+
+            if isinstance(a, (StyExpr,)):
+                a = a.eval()
+
+            if isinstance(b, (StyExpr,)):
+                b = b.eval()
+
+            return impl(a, b)
+
+    BinaryOp.__name__ = name
+    return BinaryOp
+
+
+# Numeric operators
+StyDivide = _define_binary_op("StyDivide", "/", operator.truediv)
+StyFloorDivide = _define_binary_op("StyFloorDivide", "//", operator.floordiv)
+StyMinus = _define_binary_op("StyMinus", "-", operator.sub)
+StyModulo = _define_binary_op("StyModulo", "%", operator.mod)
+StyMultiply = _define_binary_op("StyMultiply", "*", operator.mul)
+StyPlus = _define_binary_op("StyPlus", "+", operator.add)
+StyPower = _define_binary_op("StyPower", "**", operator.pow)
+
+# Logic operators
+StyAnd = _define_binary_op("StyAnd", "and", operator.and_)
+StyGreaterEqual = _define_binary_op("StyGreaterEqual", ">=", operator.ge)
+StyGreaterThan = _define_binary_op("StyGreaterThan", ">", operator.gt)
+StyLessEqual = _define_binary_op("StyLessEqual", "<=", operator.le)
+StyLessThan = _define_binary_op("StyLessThan", "<", operator.lt)
+StyOr = _define_binary_op("StyOr", "or", operator.or_)
---
+++ tests/math/test_expr.py
@@ -0,0 +1,212 @@
+import pytest
+import numpy as np
+
+from unittest import TestCase
+from stylo.math.expr import (
+    StyAnd,
+    StyDivide,
+    StyFloorDivide,
+    StyGreaterEqual,
+    StyGreaterThan,
+    StyLessEqual,
+    StyLessThan,
+    StyMinus,
+    StyModulo,
+    StyMultiply,
+    StyOr,
+    StyPlus,
+    StyPower,
+)
+from stylo.testing.math import BaseBinaryOpTest
+
+
+@pytest.mark.math
+class TestStyAnd(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyAnd` binary operation."""
+
+    def setUp(self):
+        self.operation = StyAnd
+        self.examples = [
+            (False, False, False),
+            (False, True, False),
+            (True, False, False),
+            (True, True, True),
+            (
+                np.array([True, False, True]),
+                np.array([False, True, True]),
+                np.array([False, False, True]),
+            ),
+        ]
+
+
+@pytest.mark.math
+class TestStyDivide(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyDivide` binary operation."""
+
+    def setUp(self):
+        self.operation = StyDivide
+        self.examples = [
+            (12, 1, 12),
+            (3, 2, 1.5),
+            (25, 5, 5),
+            (np.array([12, 4, 1]), np.array([4, 2, 1]), np.array([3, 2, 1])),
+        ]
+
+
+@pytest.mark.math
+class TestStyFloorDivide(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyFloorDivide` binary operation."""
+
+    def setUp(self):
+        self.operation = StyFloorDivide
+        self.examples = [
+            (12, 1, 12),
+            (3, 2, 1),
+            (25, 5, 5),
+            (np.array([12, 4, 1]), np.array([4, 2, 1]), np.array([3, 2, 1])),
+        ]
+
+
+@pytest.mark.math
+class TestStyGreaterEqual(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyGreaterEqual` binary operation."""
+
+    def setUp(self):
+        self.operation = StyGreaterEqual
+        self.examples = [
+            (1, 2, False),
+            (2, 2, True),
+            (2, 1, True),
+            (np.array([1, 2, 3]), np.array([3, 2, 1]), np.array([False, True, True])),
+        ]
+
+
+@pytest.mark.math
+class TestStyGreaterThan(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyGreaterThan` binary operation."""
+
+    def setUp(self):
+        self.operation = StyGreaterThan
+        self.examples = [
+            (1, 2, False),
+            (2, 2, False),
+            (2, 1, True),
+            (np.array([1, 2, 3]), np.array([3, 2, 1]), np.array([False, False, True])),
+        ]
+
+
+@pytest.mark.math
+class TestStyLessEqual(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyLessEqual` binary operator."""
+
+    def setUp(self):
+        self.operation = StyLessEqual
+        self.examples = [
+            (1, 2, True),
+            (2, 2, True),
+            (2, 1, False),
+            (np.array([1, 2, 3]), np.array([3, 2, 1]), np.array([True, True, False])),
+        ]
+
+
+@pytest.mark.math
+class TestStyLessThan(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyLessThan` binary operator."""
+
+    def setUp(self):
+        self.operation = StyLessThan
+        self.examples = [
+            (1, 2, True),
+            (2, 2, False),
+            (2, 1, False),
+            (np.array([1, 2, 3]), np.array([3, 2, 1]), np.array([True, False, False])),
+        ]
+
+
+@pytest.mark.math
+class TestStyMinus(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyMinus` binary operation."""
+
+    def setUp(self):
+        self.operation = StyMinus
+        self.examples = [
+            (1, 0, 1),
+            (0, 1, -1),
+            (12, 6, 6),
+            (np.array([3, 2, 1]), np.array([1, 2, 3]), np.array([2, 0, -2])),
+        ]
+
+
+@pytest.mark.math
+class TestStyModulo(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyModulo` binary operation."""
+
+    def setUp(self):
+        self.operation = StyModulo
+        self.examples = [
+            (12, 1, 0),
+            (12, 5, 2),
+            (12, 24, 12),
+            (np.array([3, 2, 1]), np.array([1, 2, 3]), np.array([0, 0, 1])),
+        ]
+
+
+@pytest.mark.math
+class TestStyMultiply(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyMultiply` binary operation."""
+
+    def setUp(self):
+        self.operation = StyMultiply
+        self.examples = [
+            (2, 1, 2),
+            (12, 0, 0),
+            (6, 6, 36),
+            (np.array([1, 2, 4]), np.array([4, 2, 1]), np.array([4, 4, 4])),
+        ]
+
+
+@pytest.mark.math
+class TestStyOr(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyOr` binary operation."""
+
+    def setUp(self):
+        self.operation = StyOr
+        self.examples = [
+            (False, False, False),
+            (False, True, True),
+            (True, False, True),
+            (True, True, True),
+            (
+                np.array([True, False, True]),
+                np.array([False, True, True]),
+                np.array([True, True, True]),
+            ),
+        ]
+
+
+@pytest.mark.math
+class TestStyPlus(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyPlus` binary operation."""
+
+    def setUp(self):
+        self.operation = StyPlus
+        self.examples = [
+            (0, 1, 1),
+            (2, 3, 5),
+            (1, -1, 0),
+            (np.array([1, 2, 3]), np.array([3, 2, 1]), np.array([4, 4, 4])),
+        ]
+
+
+@pytest.mark.math
+class TestStyPower(TestCase, BaseBinaryOpTest):
+    """Tests for the :code:`StyPower` binary operation."""
+
+    def setUp(self):
+        self.operation = StyPower
+        self.examples = [
+            (1, 2, 1),
+            (2, 3, 8),
+            (16, 0.5, 4),
+            (np.array([1, 2, 3]), np.array([1, 2, 3]), np.array([1, 4, 27])),
+        ]
