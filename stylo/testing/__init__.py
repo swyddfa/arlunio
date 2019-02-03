b911c5bc91fa740f5905791816e6dd1e51c65587
Alex Carney <alcarneyme@gmail.com>
2019-02-03 20:13:51 +0000

    Dont import old test helpers

--- stylo/testing/__init__.py
+++ stylo/testing/__init__.py
@@ -7,12 +7,3 @@ from .strategies import (  # noqa: F401
     domain_values,
     shape_mask,
 )
-from .color import BaseColorMapTest  # noqa: F401
-from .design import (  # noqa: F401
-    define_parameter_group_test,
-    define_time_dependent_parameter_group_test,
-    BasePositionTest,
-    BaseTrajectoryTest,
-)
-from .domain import define_domain_test, BaseRealDomainTest  # noqa: F401
-from .image import BaseImageTest  # noqa: F401
