fea68e8383b89477e81f7956419e39f579ed62f9
Alex Carney <alcarneyme@gmail.com>
2019-02-03 20:35:35 +0000

    Delete unused code

--- stylo/domain/transform/__init__.py
+++
@@ -1,5 +0,0 @@
-from .transform import RealDomainTransform, define_transform  # noqa: F401
-
-from .rotation import rotate  # noqa: F401
-from .shear import horizontal_shear, vertical_shear  # noqa: F401
-from .translation import translate  # noqa: F401
