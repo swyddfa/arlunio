3d57db0a3e4fe456c5695241bfa08eb99bf0fc02
Alex Carney <alcarneyme@gmail.com>
2019-07-15 18:29:04 +0100

    Improvements to collections
    - Entry points are no longer resolved on initial load, only the items
      that are explicitly referenced will be loaded

--- stylo/loaders.py
+++ stylo/loaders.py
@@ -7,7 +7,7 @@ _LOAD_CACHE = {}
 
 
 class Collection:
-    """A collection of items."""
+    """A collection of items, populate from an entry point."""
 
     def __init__(self, name, items):
         self.name = name
@@ -16,15 +16,39 @@ class Collection:
     def __repr__(self):
         return f"Collection<{self.name}, {len(self.items)} items>"
 
+    def _get(self, name):
+        """Get an item, return None if none exists."""
+        if name not in self.items:
+            logger.debug(f"Item {name} does not exist in collection {self.name}")
+            return None
+
+        item = self.items[name]
+
+        if isinstance(item, pkg_resources.EntryPoint):
+            logger.debug(f"Item {name} is an entry point reference, loading...")
+
+            real_item = item.load()
+            self.items[name] = real_item
+
+            return real_item
+
+        return item
+
     def __getitem__(self, name):
-        return self.items[name]
+        item = self._get(name)
+
+        if item is None:
+            raise KeyError(name)
+
+        return item
 
     def __getattr__(self, name):
+        item = self._get(name)
 
-        try:
-            return self.items[name]
-        except KeyError:
+        if item is None:
             raise AttributeError(name)
+
+        return item
 
     def list(self):
         """List all of the available items in the collection."""
@@ -43,12 +67,12 @@ def _load_collection(name, entry_point, docstring=None):
     if name in _LOAD_CACHE:
         return _LOAD_CACHE[name]
 
-    logger.debug(f"Building {name}")
+    logger.debug(f"Loading {name}")
     items = {}
 
     for item in pkg_resources.iter_entry_points(entry_point):
-        logger.debug(f"--> Loading {item.name}")
-        items[item.name] = item.load()
+        logger.debug(f"--> {item.module_name}.{item.name}")
+        items[item.name] = item
 
     collection = Collection(name, items)
 
