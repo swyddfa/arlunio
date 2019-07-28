8b49fa0ffd5241783d67470af57590b08a95c7e4
Alex Carney <alcarneyme@gmail.com>
2019-07-28 19:01:33 +0100

    Include the exported tutorial in the built package
    - Using the `nbtutorial` builder we now export the tutorial from the
      documentation as part of the build process
    - The exported tutorial is then copied into the `stylo.tutorial` module
      in the codebase where it is included in the package we build.
    - At the momemnt I have dropped the use of `check-manifest` since the
      tutorial is no longer part of the git repo...
    - Since we are no longer storing notebook files in the repo we can
      remove the `clean-notebooks` script from our `pre-commit` hooks. This
      also has the added bonus of removing the rather heavy `jupyter`
      dependency at the linting stage

--- stylo/doc/builder.py
+++ stylo/doc/builder.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import typing
+from pathlib import Path
 
 import attr
 import docutils.nodes as nodes
@@ -276,6 +277,8 @@ class NotebookTutorialBuilder(Builder):
 
         base, fname = os.path.split(docname)
         basedir = os.path.join(self.outdir, base)
+
+        Path(basedir, "__init__.py").touch()
         outfile = os.path.join(basedir, fname + ".ipynb")
 
         if not os.path.exists(basedir):
