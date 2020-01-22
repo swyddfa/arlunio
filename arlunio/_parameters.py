6e2bf576d9d22548eacafbb81dfbd0d272377f48
Alex Carney <alcarneyme@gmail.com>
2020-01-22 18:22:09 +0000

    wip

--- arlunio/_parameters.py
+++ arlunio/_parameters.py
@@ -12,7 +12,19 @@ class Parameter:
     META_ID: ClassVar[str] = "arlunio.parameter.attribute"
 
     def __call__(self, width: int, height: int):
-        return self._definition(width, height)
+        args = dict(self.parameters)
+
+        for name in args:
+
+            if name == "width" and args[name] == inspect.Parameter.empty:
+                args[name] = width
+                continue
+
+            if name == "height" and args[name] == inspect.Parameter.empty:
+                args[name] = height
+                continue
+
+        return self._definition(**args)
 
 
 # The default parameter collection
@@ -28,9 +40,17 @@ def parameter(f=None, *, collection=None):
     def wrapper(pdef):
 
         name = pdef.__name__
+
+        kw_only = inspect.Parameter.KEYWORD_ONLY
+        signature = inspect.signature(pdef).parameters
+
+        attrs = [attr for attr in signature.values() if attr.kind == kw_only]
+        params = [param for param in signature.values() if param not in attrs]
+
         attributes = {
             "__doc__": inspect.getdoc(pdef),
             "_definition": staticmethod(pdef),
+            "parameters": {p.name: p.annotation for p in params},
         }
 
         p = attr.s(type(name, (Parameter,), attributes))
