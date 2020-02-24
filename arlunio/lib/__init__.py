3ad6a59737fa1595d5faae805da83df23fd43c44
Alex Carney <alcarneyme@gmail.com>
2020-02-24 20:33:44 +0000

    Simplify, make better use of pillow (#193)
    **Images**
    - Use Pillow images throughout rather than our own `Image` abstraction (Closes #192 )
    - The `encode` and `save` methods on our old `Image` class are now standalone methods
    - There's no longer any need for a `arlunio._color` module, we can make use of Pillow's modes
    
    **Other**
    - Use the `appdirs` module to find the location we save the tutorial to, no longer any need for an `arlunuio._config` module
    - Expose stdlib components at the `arlunio.lib` level

---
+++ arlunio/lib/__init__.py
@@ -0,0 +1,3 @@
+from .parameters import R, T, X, Y  # noqa: F401
+from .pattern import Grid, Map, Pixelize  # noqa: F401
+from .shapes import Circle, Ellipse, Rectangle, Square, SuperEllipse  # noqa: F401
