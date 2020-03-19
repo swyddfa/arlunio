315b904aad327540082ec03215372448f38487e2
Alex Carney <alcarneyme@gmail.com>
2020-03-19 22:39:52 +0000

    Attempt to use towncrier for release notes (#204)

---
+++ scripts/write-release-notes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# It seems really fiddly to get relase notes into github
+# This does the following
+#
+# - Towncrier writes the notes for the release
+# - GitHub can't handle rst in release notes so we generate HTML with rst2html.py
+# - Strip all newlines
+# - Strip all class="x" declarations
+# - Strip all id="x" declarations
+# - Escape all '"' characters so we hopefully produce valid JSON
+towncrier --draft | rst2html.py --template=changelog/github-template.html \
+                  | tr -d '\n' \
+                  | sed 's/ class="[^"]*"//g' \
+                  | sed 's/ id="[^"]*"//g' \
+                  | sed 's.".\\".g'
