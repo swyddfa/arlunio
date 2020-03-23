9f48c7faed22ab4f495ce78843ba92893d4524b0
Alex Carney <alcarneyme@gmail.com>
2020-03-23 20:07:13 +0000

    Remove dead code

--- arlunio/__init__.py
+++ arlunio/__init__.py
@@ -1,4 +1,4 @@
-from ._core import Collection, Definition, definition  # noqa: F401
+from ._core import Definition, definition  # noqa: F401
 from ._expressions import all, any, clamp, invert, lerp, normalise  # noqa: F401
 from ._images import Image, Resolutions, colorramp, encode, fill, save  # noqa: F401
 from ._version import __version__  # noqa: F401
