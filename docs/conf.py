54bca327c5cd808295d738f3595041efaeefac13
Alex Carney <alcarneyme@gmail.com>
2020-04-20 20:43:08 +0100

    Fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = "2017-, Swyddfa Developers"
 author = "Swyddfa Developers"
 
 # The full version, including alpha/beta/rc tags
-verion = None
+version = None
 
 if "VERSION" in os.environ:
     version = os.environ["VERSION"]
