f61b6458b7d864100df467315e235315614476ba
Alex Carney <alcarneyme@gmail.com>
2020-07-09 22:02:40 +0100

    Enable "Edit on Github" in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,13 @@ gettext_compact = True
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-
+html_context = {
+    "conf_py_path": "/docs/",
+    "display_github": True,
+    "github_repo": "arlunio",
+    "github_user": "swyddfa",
+    "github_version": "develop",
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
