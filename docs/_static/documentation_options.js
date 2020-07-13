6a63c28941a682b906aa3c7c2bbd70f5bd1acc59
alcarney <alex.rugby101@gmail.com>
2020-07-13 19:36:01 +0000

    Deploying to gh-pages from develop @ ee27109447e9eb94b15ddeb0a9a8f6cdf332980e 🚀

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V0.10.0',
+    VERSION: 'V0.11.0b8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
